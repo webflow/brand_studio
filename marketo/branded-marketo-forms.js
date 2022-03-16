0c2afaee87d1c7b53fa81a404845b906354c1db1
Corey Moen <corey.moen@webflow.com>
2022-03-16 12:49:28 -0500

    Update to add functionality for managing checkboxes

--- marketo/branded-marketo-forms.js
+++ marketo/branded-marketo-forms.js
@@ -1,4 +1,7 @@
 MktoForms2.whenReady(function (form) {
     $("input[type='hidden']").parent(".mktoFormRow").css("display", "none");
-    $("textarea").parent(".mktoFieldWrap").parent(".mktoFieldDescriptor").parent(".mktoFormRow").addClass("full-width");
+    $("textarea").closest(".mktoFormRow").addClass("full-width");
+    $("input[type='checkbox']").closest(".mktoFormRow").addClass("full-width");
+    $("input[type='checkbox']").closest(".mktoFieldWrap").addClass("u-d-flex");
+    $("input[type='checkbox']").closest(".mktoFieldWrap").find(".mktoLabel").addClass("sr-only");
 });
