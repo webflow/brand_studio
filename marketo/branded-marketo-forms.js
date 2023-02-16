88d155f2076b2b6061a484b13a6dd482bc3640e1
No-Code Supply Co <corey.moen@webflow.com>
2023-02-16 09:38:02 -0600

    Allow all rich text to be full-width

--- marketo/branded-marketo-forms.js
+++ marketo/branded-marketo-forms.js
@@ -4,6 +4,7 @@ MktoForms2.whenReady(function (form) {
   $("input[type='checkbox']").closest(".mktoFormRow").addClass("full-width");
   $("input[type='checkbox']").closest(".mktoFieldWrap").addClass("u-d-flex");
   $("input[type='checkbox']").closest(".mktoFieldWrap").find(".mktoLabel").addClass("sr-only");
-  $("#terms-statement").closest(".mktoFormRow").addClass("full-width").addClass("terms");
+  $(".mktoHtmlText").closest(".mktoFormRow").addClass("full-width");
+  $("#terms-statement").closest(".mktoFormRow").addClass("terms");
   $(".mktoFormRow.terms").insertAfter(".mktoButtonRow");
 });
