2293d8e358f8a3aabf15473283b54097a1c965a9
No-Code Supply Co <corey.moen@webflow.com>
2023-02-16 09:20:09 -0600

    Update to include terms
    We will be adding the terms/conditions rich text from Marketo now so these are updates to accomodate a couple position and styling adjustments for that addition.

--- marketo/branded-marketo-forms.js
+++ marketo/branded-marketo-forms.js
@@ -1,7 +1,9 @@
 MktoForms2.whenReady(function (form) {
-    $("input[type='hidden']").parent(".mktoFormRow").css("display", "none");
-    $("textarea").closest(".mktoFormRow").addClass("full-width");
-    $("input[type='checkbox']").closest(".mktoFormRow").addClass("full-width");
-    $("input[type='checkbox']").closest(".mktoFieldWrap").addClass("u-d-flex");
-    $("input[type='checkbox']").closest(".mktoFieldWrap").find(".mktoLabel").addClass("sr-only");
+  $("input[type='hidden']").parent(".mktoFormRow").css("display", "none");
+  $("textarea").closest(".mktoFormRow").addClass("full-width");
+  $("input[type='checkbox']").closest(".mktoFormRow").addClass("full-width");
+  $("input[type='checkbox']").closest(".mktoFieldWrap").addClass("u-d-flex");
+  $("input[type='checkbox']").closest(".mktoFieldWrap").find(".mktoLabel").addClass("sr-only");
+  $(".mktoHtmlText").closest(".mktoFormRow").addClass("full-width").addClass("terms");
+  $(".mktoFormRow.terms").insertAfter(".mktoButtonRow");
 });
