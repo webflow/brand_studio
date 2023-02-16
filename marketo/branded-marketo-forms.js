3f52acabd0def84c1bde9fafe2ea7038d7632b8d
No-Code Supply Co <corey.moen@webflow.com>
2023-02-16 09:30:43 -0600

    Other adjustments to terms addition

--- marketo/branded-marketo-forms.js
+++ marketo/branded-marketo-forms.js
@@ -4,6 +4,6 @@ MktoForms2.whenReady(function (form) {
   $("input[type='checkbox']").closest(".mktoFormRow").addClass("full-width");
   $("input[type='checkbox']").closest(".mktoFieldWrap").addClass("u-d-flex");
   $("input[type='checkbox']").closest(".mktoFieldWrap").find(".mktoLabel").addClass("sr-only");
-  $(".mktoHtmlText").closest(".mktoFormRow").addClass("full-width").addClass("terms");
+  $("#terms-statement").closest(".mktoFormRow").addClass("full-width").addClass("terms");
   $(".mktoFormRow.terms").insertAfter(".mktoButtonRow");
 });
