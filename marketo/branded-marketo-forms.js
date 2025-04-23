722cd572c6f94705b790bda8d4cbe2b10fe0dffd
Corey Moen <84108141+webflow-corey@users.noreply.github.com>
2025-04-23 09:10:34 -0500

    Webflow Logged in User ID Handling
    Adding a way to automatically populate the hidden "Webflow_USER_ID__c" input on any Marketo form with the logged in user ID on page load.

--- marketo/branded-marketo-forms.js
+++ marketo/branded-marketo-forms.js
@@ -3,10 +3,29 @@ MktoForms2.whenReady(function (form) {
   $("textarea").closest(".mktoFormRow").addClass("full-width");
   $("input[type='checkbox']").closest(".mktoFormRow").addClass("full-width");
   $("input[type='checkbox']").closest(".mktoFieldWrap").addClass("u-d-flex");
-  $("input[type='checkbox']").closest(".mktoFieldWrap").find(".mktoLabel").addClass("sr-only");
+  $("input[type='checkbox']")
+    .closest(".mktoFieldWrap")
+    .find(".mktoLabel")
+    .addClass("sr-only");
   $(".mktoHtmlText").closest(".mktoFormRow").addClass("full-width");
   $(".terms-statement").closest(".mktoFormRow").addClass("terms");
   $(".mktoForm").each(function () {
     $(this).find(".mktoFormRow.terms").appendTo($(this));
   });
+
+  // Update Webflow User hidden input if logged into Webflow
+  if (typeof wf_utils !== "undefined" && wf_utils.getUser) {
+    wf_utils.getUser((user) => {
+      // Check if user object exists
+      if (!user) {
+        return;
+      }
+      // Extract the user ID from the user object
+      const userId = user.id;
+      // Find all input elements with name "Webflow_USER_ID__c" and set their value
+      $('input[name="Webflow_USER_ID__c"]').each(function () {
+        $(this).val(userId);
+      });
+    });
+  }
 });
